b9c64db97d22809fe477d31d838fd8a44720d26e
Kameron Johnson <kamjohn79.kdj@gmail.com>
2025-01-31 09:24:40 -0600

    working on lose, win screens

--- Gravity-Falls-Game-Attempt/src/components/OutcomeDisplay.jsx
+++ Gravity-Falls-Game-Attempt/src/components/OutcomeDisplay.jsx
@@ -4,6 +4,11 @@ import { motion } from "framer-motion";
 import "./OutcomeDisplay.css";
 
 function OutComeDisplay({ gameState, onReset }) {
+  const backgroundImage = gameState === "lost" ? `url(${lost})` : `url(${win})`;
+  const backgroundColor = gameState === "lost" ? "red" : "blue";
+  const message = gameState === "lost" ? "You lost!" : "You won!";
+  const messageClass = gameState === "lost" ? "message-lost" : "message-won";
+
   return (
     <motion.div
       className="outcome-display"
@@ -12,9 +17,12 @@ function OutComeDisplay({ gameState, onReset }) {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5, ease: "easeInOut" }}
       exit={{ opacity: 0 }}
+      style={{ backgroundImage, backgroundColor }}
     >
-      <img src={gameState === "lost" ? lost : win} alt="Game Over" />
-      <button onClick={onReset}>Reset</button>
+      <div>
+        <p className={messageClass}>{message}</p>
+        <button onClick={onReset}>Reset</button>
+      </div>
     </motion.div>
   );
 }
